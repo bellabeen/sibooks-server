b7a6e9c5a9cc3656ddd11be51410b5ec460a2772
bellabeen <rootsquid@gmail.com>
2019-11-28 13:59:11 +0700

    Update

--- conf/config.php
+++ conf/config.php
@@ -1,7 +1,8 @@
 <?php
-define("DB_HOST","localhost");
-define("DB_USER","bellabeen");
-define("DB_PASSWORD","kepoajalu");
-define("DB_NAME","sibook");
+define("DB_HOST","fdb22.awardspace.net");
+define("DB_USER","3183666_sibooks");
+define("DB_PASSWORD","Kepoajalu1851
+");
+define("DB_NAME","3183666_sibooks");
 
 ?>
